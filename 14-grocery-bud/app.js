59214a08a49dd4021d295632aa38f15fb3f97361
kimara <goldknow456@gmail.com>
2023-02-01 16:57:36 +0900

    day 4

--- 14-grocery-bud/app.js
+++ 14-grocery-bud/app.js
@@ -23,8 +23,202 @@ window.addEventListener("DOMContentLoaded", setupItems);
 // ****** functions **********
 
 // add item
+function addItem(e) {
+    e.preventDefault();
 
+    const value = grocery.value;
+    const id = new Date().getTime().toString();
 
+    if (value !== "" && !editFlag) {
+        const element = document.createElement("article");
+        let attr = document.createAttribute("data-id");
+        attr.value = id;
+        element.setAttributeNode(attr);
+        element.classList.add("grocery-item");
+        element.innerHTML = `<p class="title">${value}</p>
+                <div class="btn-container">
+                    <!-- edit btn -->
+                    <button type="button" class="edit-btn">
+                        <i class="fas fa-edit"></i>
+                    </button>
+                    <!-- delete btn -->
+                    <button type="button" class="delete-btn">
+                        <i class="fas fa-trash"></i>
+                    </button>
+                </div>
+            `;
+        // 두 버튼에 이벤트 리스너를 추가합니다.
+        const deleteBtn = element.querySelector(".delete-btn");
+        deleteBtn.addEventListener("click", deleteItem);
+        const editBtn = element.querySelector(".edit-btn");
+        editBtn.addEventListener("click", editItem);
 
+        // 자식 추가
+        list.appendChild(element);
+        // 디스플레이 경고
+        displayAlert("아이템을 리스트에 추가해주세요.", "success");
+        // 컨테이너 표시
+        container.classList.add("show-container");
+        // 로컬 저장소 설정
+        addToLocalStorage(id, value);
+        // 기본값으로 다시 설정
+        setBackToDefault();
+    } else if (value !== "" && editFlag) {
+        editElement.innerHTML = value;
+        displayAlert("값 변경", "success");
 
+        // 로컬 저장소 편집
+        editLocalStorage(editID, value);
+        setBackToDefault();
+    } else {
+        displayAlert("값을 입력해주세요.", "danger");
+    }
+}
 
+// 디스플레이 경고
+function displayAlert(text, action) {
+    alert.textContent = text;
+    alert.classList.add(`alert-${action}`);
+    // 경고 삭제
+    setTimeout(function () {
+        alert.textContent = "";
+        alert.classList.remove(`alert-${action}`);
+    }, 1000);
+}
+
+// clear item
+function clearItems() {
+    const items = document.querySelectorAll(".grocery-item");
+    if (items.length > 0) {
+        items.forEach(function (item) {
+            list.removeChild(item);
+        });
+    }
+
+    container.classList.remove("show-container");
+    displayAlert("리스트 없음", "danger");
+    setBackToDefault();
+    localStorage.removeItem("list");
+}
+
+// delete item
+function deleteItem(e) {
+    const element = e.currentTarget.parentElement.parentElement;
+    const id = element.dataset.id;
+
+    list.removeChild(element);
+
+    if (list.children.length === 0) {
+        container.classList.remove("show-container");
+    }
+    displayAlert("아이템 삭제", "danger");
+
+    setBackToDefault();
+
+    //로컬 저장소에서 제거
+    removeFromLocalStorage(id);
+}
+
+// edit item
+function editItem(e) {
+    const element = e.currentTarget.parentElement.parentElement;
+    // 편집 항목 설정
+    editElement = e.currentTarget.parentElement.previousElementSibling;
+    // 양식 값 설정
+    grocery.value = editElement.innerHTML;
+    editFlag = true;
+    editID = element.dataset.id;
+    //
+    submitBtn.textContent = "edit";
+}
+
+// backt를 기본값으로 설정
+function setBackToDefault() {
+    grocery.value = "";
+    editFlag = false;
+    editID = "";
+    submitBtn.textContent = "submit";
+}
+
+// ****** local storage **********
+
+// 로컬 저장소에 추가
+function addToLocalStorage(id, value) {
+    const grocery = { id, value };
+    let items = getLocalStorage();
+    items.push(grocery);
+    localStorage.setItem("list", JSON.stringify(items));
+}
+
+function getLocalStorage() {
+    return localStorage.getItem("list") ? JSON.parse(localStorage.getItem("list")) : [];
+}
+
+function removeFromLocalStorage(id) {
+    let items = getLocalStorage();
+
+    items = items.filter(function (item) {
+        if (item.id !== id) {
+            return item;
+        }
+    });
+
+    localStorage.setItem("list", JSON.stringify(items));
+}
+
+function editLocalStorage(id, value) {
+    let items = getLocalStorage();
+
+    items = items.map(function (item) {
+        if (item.id === id) {
+            item.value = value;
+        }
+        return item;
+    });
+
+    localStorage.setItem("list", JSON.stringify(items));
+}
+
+// SETUP LOCALSTORAGE.REMOVEITEM('LIST');
+
+// ****** 설정 항목 **********
+function setupItems() {
+    let items = getLocalStorage();
+
+    if (items.length > 0) {
+        items.forEach(function (item) {
+            createListItem(item.id, item.value);
+        });
+
+        container.classList.add("show-container");
+    }
+}
+
+function createListItem(id, value) {
+    const element = document.createElement("article");
+    let attr = document.createAttribute("data-id");
+    attr.value = id;
+
+    element.setAttributeNode(attr);
+    element.classList.add("grocery-item");
+    element.innerHTML = `<p class="title">${value}</p>
+            <div class="btn-container">
+                <!-- edit btn -->
+                <button type="button" class="edit-btn">
+                    <i class="fas fa-edit"></i>
+                </button>
+                <!-- delete btn -->
+                <button type="button" class="delete-btn">
+                    <i class="fas fa-trash"></i>
+                </button>
+            </div>
+        `;
+    //두 버튼에 이벤트 리스너를 추가합니다.
+    const deleteBtn = element.querySelector(".delete-btn");
+    deleteBtn.addEventListener("click", deleteItem);
+    const editBtn = element.querySelector(".edit-btn");
+    editBtn.addEventListener("click", editBtn);
+
+    //자식 추가
+    list.appendChild(element);
+}
