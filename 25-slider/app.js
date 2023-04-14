b1655a9e98e38361ac05f7995d9379014c3f1126
kimara <goldknow456@gmail.com>
2023-04-14 14:13:01 +0900

    day 9

---
+++ 25-slider/app.js
@@ -0,0 +1,89 @@
+import data from "./data.js";
+
+const container = document.querySelector(".slide-container");
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
+
+// data가 1이면 버튼 숨기기
+if (data.length === 1) {
+    nextBtn.style.display = "none";
+    prevBtn.style.display = "none";
+}
+
+// data가 2이면 슬라이드 사본 추가
+let people = [...data];
+
+if (data.length === 2) {
+    people = [...data, ...data];
+}
+
+container.innerHTML = people
+    .map((person, slideIndex) => {
+        const { img, name, job, text } = person;
+        let position = "next";
+
+        if (slideIndex === 0) {
+            position = "active";
+        }
+
+        if (slideIndex === people.length - 1) {
+            position = "last";
+        }
+
+        if (data.length <= 1) {
+            position = "active";
+        }
+
+        return ` <article class="slide ${position}">
+        <img src="${img}" class="img" alt="${name}" />
+        <h4>${name}</h4>
+        <p class="title">${job}</p>
+        <p class="text">${text}</p>
+        <div class="quote-icon">
+            <i class="fas fa-quote-right"></i>
+        </div>
+    </article> `;
+    })
+    .join("");
+
+const startSlider = (type) => {
+    // 세 개의 슬라이드를 모두 활성화하고 마지막으로 다음
+    const active = document.querySelector(".active");
+    const last = document.querySelector(".last");
+    let next = active.nextElementSibling;
+
+    if (!next) {
+        next = container.firstElementChild;
+    }
+
+    active.classList.remove("active");
+    last.classList.remove("last");
+    next.classList.remove("next");
+
+    if (type === "prev") {
+        active.classList.add("next");
+        last.classList.add("active");
+        next = last.previousElementSibling;
+
+        if (!next) {
+            next = container.lastElementChild;
+        }
+
+        next.classList.remove("next");
+        next.classList.add("last");
+
+        return;
+    }
+
+    active.classList.add("last");
+    last.classList.add("next");
+    next.classList.add("active");
+};
+
+nextBtn.addEventListener("click", () => {
+    startSlider();
+});
+
+prevBtn.addEventListener("click", () => {
+    startSlider("prev");
+});
