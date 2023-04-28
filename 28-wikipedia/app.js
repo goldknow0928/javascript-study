f20bbe60d112f6b2adc901d135bdcf128e484994
kimara <goldknow456@gmail.com>
2023-04-28 14:59:43 +0900

    day 12

---
+++ 28-wikipedia/app.js
@@ -0,0 +1,52 @@
+const url = "https://en.wikipedia.org/w/api.php?action=query&list=search&srlimit=20&format=json&origin=*&srsearch=";
+
+const formDOM = document.querySelector(".form");
+const inputDOM = document.querySelector(".form-input");
+const resultsDOM = document.querySelector(".results");
+
+formDOM.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const value = inputDOM.value;
+
+    if (!value) {
+        resultsDOM.innerHTML  = `<div class="error">유효한 검색어를 입력하십시오</div>`;
+        return;
+    }
+
+    fetchPages(value);
+});
+
+const fetchPages = async (searchValue) => {
+    resultsDOM.innerHTML  = `<div class="loading"></div>`;
+
+    try {
+        const response = await fetch(`${url}${searchValue}`);
+        const data = await response.json();
+        const results = data.query.search;
+
+        if (results.length < 1) {
+            resultsDOM.innerHTML  = `<div class="error">일치하는 결과가 없습니다. 다시 시도해 주세요</div>`;
+
+            return;
+        }
+        renderResults(results);
+    } catch (error) {
+        resultsDOM.innerHTML  = `<div class="error">오류가있었습니다...</div>`;
+    }
+};
+
+const renderResults = (list) => {
+    const cardsList = list
+        .map((item) => {
+            const { title, snippet, pageid } = item;
+
+            return `<a href=http://en.wikipedia.org/?curid=${pageid} target="_blank">
+            <h4>${title}</h4>
+            <p>${snippet}</p>
+        </a>`;
+        })
+        .join("");
+
+    resultsDOM.innerHTML  = `<div class="articles">${cardsList}</div>`;
+};
