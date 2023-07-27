20131457fbccb53d3c2f4fad6cc18844c3da082a
kimara <goldknow456@gmail.com>
2023-07-27 15:56:59 +0900

    Add: day 16

---
+++ step2/background-slider/script.js
@@ -0,0 +1,39 @@
+const body = document.body;
+const slides = document.querySelectorAll(".slide");
+const leftBtn = document.getElementById("left");
+const rightBtn = document.getElementById("right");
+
+let activeSlide = 0;
+
+leftBtn.addEventListener("click", () => {
+    activeSlide--;
+
+    if (activeSlide < 0) {
+        activeSlide = slides.length - 1;
+    }
+
+    bodyBg();
+    setSlideActive();
+});
+
+rightBtn.addEventListener("click", () => {
+    activeSlide++;
+
+    if (activeSlide > slides.length - 1) {
+        activeSlide = 0;
+    }
+
+    bodyBg();
+    setSlideActive();
+});
+
+bodyBg();
+
+function bodyBg() {
+    body.style.backgroundImage = slides[activeSlide].style.backgroundImage;
+}
+
+function setSlideActive() {
+    slides.forEach((slide) => slide.classList.remove("active"));
+    slides[activeSlide].classList.add("active");
+}
